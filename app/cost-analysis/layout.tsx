a444d7b3a13d90f102ed845ab8677c01b4c595aa
Kedaar Rentachintala <kedaarnr@berkeley.edu>
2025-05-27 17:05:18 -0700

    localStorage error fix

--- app/cost-analysis/layout.tsx
+++ app/cost-analysis/layout.tsx
@@ -1,3 +1,15 @@
-export { default } from "../dashboard/layout";
+"use client";
+import { useEffect } from "react";
+import DashboardLayout from "../dashboard/layout";
 
-localStorage.removeItem("ss_chat_history");
+export default function CostAnalysisLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    localStorage.removeItem("ss_chat_history");
+  }, []);
+
+  return <DashboardLayout>{children}</DashboardLayout>;
+}
